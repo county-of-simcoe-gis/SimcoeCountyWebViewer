0ea9e1af974f39692da48edec9f355d8852beda9
Al Proulx <alain.proulx@hotmail.com>
2019-07-09 15:24:55 -0400

    Fixed MyMaps message when there are no features

--- src/sidebar/components/mymaps/MyMapsItems.jsx
+++ src/sidebar/components/mymaps/MyMapsItems.jsx
@@ -1,35 +1,30 @@
-import React, { Component} from 'react';
-import { CSSTransitionGroup } from 'react-transition-group'
-//import {CSSTransitionGroup} from 'react-transition-group/CSSTransitionGroup';
-//import CSSTransitionGroup from 'react-addons-css-transition-group';
-//import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import React, { Component } from "react";
 import "./MyMapsItems.css";
 
 class MyMapsItems extends Component {
-  state = { 
-  }
+  state = {};
 
-  render() { 
-    return ( 
+  render() {
+    return (
       <div className="sc-container sc-mymaps-item-container">
         <div className="sc-mymaps-items-container-header">
-            <img src={images["myItemsIcon.png"]} alt="My Maps Icon"/>
+          <img src={images["myItemsIcon.png"]} alt="My Maps Icon" />
         </div>
-        <div className={this.props.children.length === 0 ? "sc-mymaps-items-no-data" : "sc-hidden"}>There are currently no items to display. Please use the drawing tools above to create your own personal map item.</div>
-        <div>
-        {this.props.children}
+        <div className={this.props.children.props.children.length === 0 ? "sc-mymaps-items-no-data" : "sc-hidden"}>
+          There are currently no items to display. Please use the drawing tools above to create your own personal map item.
         </div>
+        <div>{this.props.children}</div>
       </div>
-     );
+    );
   }
 }
 
 export default MyMapsItems;
 
 // IMPORT ALL IMAGES
-const images = importAllImages(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+const images = importAllImages(require.context("./images", false, /\.(png|jpe?g|svg)$/));
 function importAllImages(r) {
-    let images = {};
-    r.keys().map((item, index) => images[item.replace('./', '')] = r(item));
-    return images;
-  }
+  let images = {};
+  r.keys().map((item, index) => (images[item.replace("./", "")] = r(item)));
+  return images;
+}
