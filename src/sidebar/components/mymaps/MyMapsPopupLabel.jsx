b2ca5ee77b3cc85dc04414d7d1ff4c9722310c11
Al Proulx <alain.proulx@hotmail.com>
2019-07-10 11:18:23 -0400

    Fixed popup checkbox

--- src/sidebar/components/mymaps/MyMapsPopupLabel.jsx
+++ src/sidebar/components/mymaps/MyMapsPopupLabel.jsx
@@ -1,67 +1,64 @@
-
-import React, { Component} from 'react';
+import React, { Component } from "react";
 import * as helpers from "../../../helpers/helpers";
 import * as myMapsHelpers from "./myMapsHelpers";
 import "./MyMapsPopupLabel.css";
-import OLOverlayCheckbox from '../../../helpers/OLOverlayCheckbox.jsx'
+import OLOverlayCheckbox from "../../../helpers/OLOverlayCheckbox.jsx";
 import Slider from "rc-slider";
-import {MouseWheelZoom} from 'ol/interaction'
+import { MouseWheelZoom } from "ol/interaction";
 
 class MyMapsPopupLabel extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
 
     this.sliderMin = 0;
     this.sliderMax = 360;
 
     this.state = {
       label: props.item.label,
-      sliderValue: this.props.item.labelRotation,
-    }
+      sliderValue: this.props.item.labelRotation
+    };
   }
 
   // USING REFS TO LISTEN TO CHANGES FROM MAIN MYMAPS COMPONENT
   componentDidMount() {
-    this.props.onRef(this)
+    this.props.onRef(this);
   }
   componentWillUnmount() {
-    this.props.onRef(undefined)
+    this.props.onRef(undefined);
   }
 
   // HANDLE PARENT MAKING LABEL CHANGE
   parentLabelChange = (itemInfo, newLabel) => {
-    if (itemInfo.id === this.props.item.id){
-      this.setState({label: newLabel});
+    if (itemInfo.id === this.props.item.id) {
+      this.setState({ label: newLabel });
     }
-  }
+  };
 
   // HANDLE LABEL CHANGE IN POPUP
-  onLabelChange = (evt) => {
-    this.setState({label: evt.target.value});
+  onLabelChange = evt => {
+    this.setState({ label: evt.target.value });
     this.props.onLabelChange(this.props.item, evt.target.value);
-  }
+  };
 
-  onLabelVisibilityChange = (checked) => {
-    this.props.onLabelVisibilityChange(this.props.item, checked);   
-  }
+  onLabelVisibilityChange = checked => {
+    this.props.onLabelVisibilityChange(this.props.item, checked);
+  };
 
   // THIS IS REQUIRED WHEN CHANGING LABEL FROM POPUP
   componentWillReceiveProps(nextProps) {
-    if (nextProps.item.label !== this.state.label)
-      this.setState({label: nextProps.item.label});   
+    if (nextProps.item.label !== this.state.label) this.setState({ label: nextProps.item.label });
   }
 
-  
   // SLIDER CHANGE EVENT
   onSliderChange = evt => {
-    this.setState({sliderValue: evt.target.value});
+    this.setState({ sliderValue: evt.target.value });
     this.props.onLabelRotationChange(this.props.item, evt.target.value);
   };
-  
-  render() { 
+
+  render() {
     return (
       <div>
-        {/* <div className="sc-mymaps-popup-label-toggler">
+        <div className="sc-mymaps-popup-label-toggler">
           <OLOverlayCheckbox
             label={"Show Label"}
             defaultChecked={this.props.item.labelVisible}
@@ -69,14 +66,13 @@ class MyMapsPopupLabel extends Component {
               this.onLabelVisibilityChange(checked);
             }}
           />
-        <div className="sc-mymaps-popup-slider">
-          <input type="range" min={this.sliderMin} max={this.sliderMax} value={this.state.sliderValue} step="1"  onChange={this.onSliderChange}></input>
-          <label className="sc-mymaps-popup-slider-label">Rotate</label>
+          <div className="sc-mymaps-popup-slider">
+            <input type="range" min={this.sliderMin} max={this.sliderMax} value={this.state.sliderValue} step="1" onChange={this.onSliderChange} />
+            <label className="sc-mymaps-popup-slider-label">Rotate</label>
+          </div>
         </div>
-
-        </div> */}
         <div>
-          <input className="sc-mymaps-popup-label-input" type="text"  value={this.state.label} onChange={this.onLabelChange} />
+          <input className="sc-mymaps-popup-label-input" type="text" value={this.state.label} onChange={this.onLabelChange} />
         </div>
       </div>
     );
