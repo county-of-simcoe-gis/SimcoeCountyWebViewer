2174d840210e870cdbb89d7b93561869bc8157a8
Al Proulx <alain.proulx@hotmail.com>
2019-07-10 10:02:07 -0400

    Added Floating Menu to Tools button

--- src/helpers/FloatingMenu.jsx
+++ src/helpers/FloatingMenu.jsx
@@ -1,8 +1,7 @@
-
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import Menu, { SubMenu, Item as MenuItem, Divider } from 'rc-menu';
-import 'rc-menu/assets/index.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import Menu, { SubMenu, Item as MenuItem, Divider } from "rc-menu";
+import "rc-menu/assets/index.css";
 import "./FloatingMenu.css";
 
 // PROPER USE OF THIS COMPONENT IS TO USE A PORTAL.  HAVE A LOOK AT MyMapsItem FOR AN EXAMPLE.
@@ -14,44 +13,38 @@ class FloatingMenu extends Component {
       styleMode: this.props.styleMode === undefined ? "right" : this.props.styleMode,
       style: {
         position: "absolute",
-        zIndex: 1000,
+        zIndex: 10000,
         top: this.props.buttonEvent.pageY,
         left: this.props.buttonEvent.pageX,
         backgroundColor: "white",
         width: "180px"
-      },
-    }
+      }
+    };
   }
 
   componentDidMount() {
-
-
-    this.getStyle((style) => {
+    this.getStyle(style => {
       this.setState({ style: style });
     });
-
-
-    //console.log(ReactDOM.findDOMNode(this).offsetHeight);
-    //console.log(this.isElementInViewport(ReactDOM.findDOMNode(this)))
-    //console.log(this.isElementInViewport(document.getElementById("portal-root")));
-
-    // if (this.container !== undefined)
-    //   console.log(this.container.offsetHeight)
-
-    // CHECK IF IN VIEWPORT
-    //console.log(this.isElementInViewport(this.container))
-    // if (!this.isElementInViewport(this.container)){
-    //   console.log("reset")
-    //   this.setState({styleMode: "left"});
-    //   this.forceUpdate();
-    // }
 
     // CLICK ANYWHERE ELSE WILL CLOSE MENU
     this.clickEvent = document.body.addEventListener(
       "click",
       evt => {
-        if (typeof evt.target.className === "string" && (evt.target.className.indexOf("rc-menu") > -1 || evt.target.className.indexOf("sc-floating-menu-") > -1))
-          return;
+        if (typeof evt.target.className === "string") {
+          // IGNORE CLASSNAMES
+          let found = false;
+          if (this.props.classNamesToIgnore !== undefined) {
+            evt.target.className.split(" ").forEach(className => {
+              if (this.props.classNamesToIgnore.includes(className)) {
+                found = true;
+                return;
+              }
+            });
+          }
+
+          if (evt.target.className.indexOf("rc-menu") > -1 || evt.target.className.indexOf("sc-floating-menu-") > -1 || found) return;
+        }
 
         if (this.container !== null && !this.container.contains(evt.target)) {
           this.setState({ isVisible: false });
@@ -61,28 +54,18 @@ class FloatingMenu extends Component {
     );
   }
 
-  // isElementInViewport(container) {
-  //   const top = container.getBoundingClientRect().bottom;
-  //   const offset = 0;
-  //   return (top + offset) >= 0 && (top - offset) <= window.innerHeight;
-  // }
-
   isElementInViewport(el) {
-
     var rect = el.getBoundingClientRect();
     return (
       rect.top >= 0 &&
       rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) && /*or $(window).height() */
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) /*or $(window).height() */ &&
       rect.right <= (window.innerWidth || document.documentElement.clientWidth) /*or $(window).width() */
     );
   }
 
-  getStyle = (callback) => {
-
-    if (this.state === undefined || !this.state.isVisible)
-      callback({ display: "none" });
-
+  getStyle = callback => {
+    if (this.state === undefined || !this.state.isVisible) callback({ display: "none" });
 
     let yOffset = 0;
     let style = null;
@@ -93,7 +76,6 @@ class FloatingMenu extends Component {
           if (!isInViewport) {
             yOffset = this.container.offsetHeight;
           }
-
         }
       }
 
@@ -108,22 +90,18 @@ class FloatingMenu extends Component {
         width: "180px"
       };
 
-
-      if (this.state.isVisible)
-        callback(style);
-
+      if (this.state.isVisible) callback(style);
     });
-
-  }
+  };
 
   handleSelect = () => {
     this.setState({ isVisible: false });
-  }
+  };
 
-  handleClick = (info) => {
-    this.props.onMenuItemClick(info.key)
+  handleClick = info => {
+    this.props.onMenuItemClick(info.key);
     this.setState({ isVisible: false });
-  }
+  };
 
   render() {
     if (!this.state.isVisible) {
@@ -131,14 +109,8 @@ class FloatingMenu extends Component {
     }
 
     return (
-      <div className="sc-floating-menu-toolbox-menu-container" style={this.state.style} ref={container => this.container = container}>
-        <Menu
-          onSelect={this.handleSelect}
-          defaultActiveFirst
-          onClick={this.handleClick}
-          onTitleClick={this.editMove}
-          className="sc-floating-menu-toolbox-menu"
-        >
+      <div className="sc-floating-menu-toolbox-menu-container" style={this.state.style} ref={container => (this.container = container)}>
+        <Menu onSelect={this.handleSelect} defaultActiveFirst onClick={this.handleClick} onTitleClick={this.editMove} className="sc-floating-menu-toolbox-menu">
           {this.props.children}
         </Menu>
       </div>
@@ -149,14 +121,18 @@ class FloatingMenu extends Component {
 export default FloatingMenu;
 
 // IMPORT ALL IMAGES
-const images = importAllImages(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+const images = importAllImages(require.context("./images", false, /\.(png|jpe?g|svg)$/));
 function importAllImages(r) {
   let images = {};
-  r.keys().map((item, index) => images[item.replace('./', '')] = r(item));
+  r.keys().map((item, index) => (images[item.replace("./", "")] = r(item)));
   return images;
 }
 
 export function FloatingMenuItem(props) {
-  return <div className="sc-floating-menu-toolbox-menu-icon-label-container"><img className="sc-floating-menu-toolbox-menu-icon" src={images[props.imageName]}></img><label className="sc-floating-menu-toolbox-menu-label">{props.label}</label></div>;
+  return (
+    <div className="sc-floating-menu-toolbox-menu-icon-label-container">
+      <img className="sc-floating-menu-toolbox-menu-icon" src={images[props.imageName]} />
+      <label className="sc-floating-menu-toolbox-menu-label">{props.label}</label>
+    </div>
+  );
 }
-
