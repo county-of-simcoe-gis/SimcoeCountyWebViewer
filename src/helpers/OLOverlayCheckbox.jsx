caefb4215de2b6c682f15b7075b1ca6150c8dd7e
Al Proulx <alain.proulx@hotmail.com>
2019-07-17 15:39:32 -0400

    OL Overlay Bug Overhaul

--- src/helpers/OLOverlayCheckbox.jsx
+++ src/helpers/OLOverlayCheckbox.jsx
@@ -1,61 +1,30 @@
 // OL HAS A NASTY BUG WHERE EVENTS DONT FIRE ON OVERLAYS
 // React and its onChange event was not firing.  Tried many different methods.
-// This component accepts props without using state and is forced to update from props.
+// Parent needs to force update because it's uncontrolled and using defaultValue
 // Spent too much time already and this works.  Any cleaner ideas?
 
 import React from "react";
 import * as helpers from "./helpers";
 
-class OLOverlayCheckbox extends React.Component {
-  onCheckboxClick = evt => {
-    const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
-    if (isSafari) {
-      // SAFARI IS SPECIAL
-      const type = evt.target.tagName.toLowerCase();
-      if (type === "label") {
-        document.getElementById("sc-ol-checkbox").checked = !this.node.checked;
-        this.props.parentClickHandler(this.node.checked);
-      } else {
-        this.props.parentClickHandler(!this.node.checked);
-      }
-    } else {
-      this.props.parentClickHandler(!this.node.checked);
-    }
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
-    document.getElementById("map").focus();
-  };
+const OLOverlayCheckbox = props => {
+  return (
+    <label
+      style={{ WebkitTouchCallout: "none", WebkitUserSelect: "none", KhtmlUserSelect: "none", MozUserSelect: "none", MsUserSelect: "none", UserSelect: "none" }}
+      onMouseUp={helpers.convertMouseUpToClick}
+      onClick={evt => {
+        var chk = document.getElementById(props.id);
+        props.onCheckboxClick(chk.checked);
 
-  render() {
-    // FORCE CHECKBOX UPDATE
-    if (this.node !== undefined) {
-      if (this.node.checked !== this.props.defaultChecked) this.node.checked = this.props.defaultChecked;
-    }
-
-    return (
-      <label htmlFor="sc-ol-checkbox" onMouseUp={helpers.convertMouseUpToClick} onClick={this.onCheckboxClick}>
-        <input
-          id="sc-ol-checkbox"
-          contentEditable="false"
-          style={{ verticalAlign: "middle" }}
-          type="checkbox"
-          defaultChecked={this.props.defaultChecked}
-          ref={input => {
-            this.node = input;
-          }}
-        />
-        {this.props.label}
-      </label>
-      //   <input
-      //   id="sc-ol-checkbox"
-      //   style={{verticalAlign: "middle"}}
-      //   type="checkbox"
-      //   defaultChecked={this.props.defaultChecked}
-      //   onClick={this.onCheckboxClick}
-      //   onMouseUp={helpers.convertMouseUpToClick}
-      //   ref={(input) => { this.node = input; }}
-      // />
-    );
-  }
-}
+        // SAFARI IS SPECIAL
+        if (evt.target.id !== props.id && isSafari) chk.checked = !chk.checked;
+      }}
+    >
+      <input style={isSafari ? { position: "relative" } : { position: "relative", top: "1.5px" }} id={props.id} type="checkbox" defaultChecked={props.checked} readOnly />
+      {props.label}
+    </label>
+  );
+};
 
 export default OLOverlayCheckbox;
