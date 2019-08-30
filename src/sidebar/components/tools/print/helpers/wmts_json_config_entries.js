6586f4e6bf2eb31b9959709eddfee95b18c156cd
Sheldon Small <ssmall@realdecoy.com>
2019-08-30 18:50:07 -0500

    updated basemap configs

--- src/sidebar/components/tools/print/helpers/wmts_json_config_entries.js
+++ src/sidebar/components/tools/print/helpers/wmts_json_config_entries.js
@@ -1,5 +1,7 @@
 import Ortho_2018_Cache from '../wmtsJsonConfig/Ortho_2018_Cache.json';
-import Public_Bathymetry_Cache from '../wmtsJsonConfig/Public_Bathymetry_Cache.json';
+import Streets_Cache from '../wmtsJsonConfig/Streets_Cache.json';
+import Ortho_2016_Cache from '../wmtsJsonConfig/Ortho_2016_Cache.json';
+import Bathymetry_Cache from '../wmtsJsonConfig/Bathymetry_Cache.json';
 import World_Imagery from '../wmtsJsonConfig/World_Imagery.json';
 
-export default {Ortho_2018_Cache, Public_Bathymetry_Cache, World_Imagery};
+export default {Streets_Cache, Ortho_2016_Cache, Bathymetry_Cache, World_Imagery};
