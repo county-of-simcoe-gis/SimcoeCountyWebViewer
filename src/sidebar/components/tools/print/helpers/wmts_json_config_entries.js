4e1fd564682a636703dde3956d9c29d64fa126ee
Sheldon <geotechvision.dev@gmail.com>
2019-09-02 23:56:52 -0500

    added ortho 2018 config.json to basemap entries

--- src/sidebar/components/tools/print/helpers/wmts_json_config_entries.js
+++ src/sidebar/components/tools/print/helpers/wmts_json_config_entries.js
@@ -4,4 +4,4 @@ import Ortho_2016_Cache from '../wmtsJsonConfig/Ortho_2016_Cache.json';
 import Bathymetry_Cache from '../wmtsJsonConfig/Bathymetry_Cache.json';
 import World_Imagery from '../wmtsJsonConfig/World_Imagery.json';
 
-export default {Streets_Cache, Ortho_2016_Cache, Bathymetry_Cache, World_Imagery};
+export default {Streets_Cache, Ortho_2018_Cache, Ortho_2016_Cache, Bathymetry_Cache, World_Imagery};
