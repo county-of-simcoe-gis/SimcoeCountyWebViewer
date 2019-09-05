773e6d8aa94bafe8f0588d07aa7113ab71f5c116
Al Proulx <alain.proulx@hotmail.com>
2019-09-05 15:51:36 -0400

    Added new Tool - External Services

--- src/sidebar/PanelComponent.jsx
+++ src/sidebar/PanelComponent.jsx
@@ -1,56 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./PanelComponent.css";
 
-class PanelComponent  extends Component {
-    state = {  }
+class PanelComponent extends Component {
+  state = {};
 
-    componentDidMount(){
-      // LISTEN FOR CLOSE FROM OTHER COMPONENTS (e.g. MENU BUTTON)
-      window.emitter.addListener('closeToolsOrThemes', (type) => {
-        if (type === this.props.type)
-          this.props.onClose();
-      });
-    }
+  componentDidMount() {
+    // LISTEN FOR CLOSE FROM OTHER COMPONENTS (e.g. MENU BUTTON)
+    window.emitter.addListener("closeToolsOrThemes", type => {
+      if (type === this.props.type) this.props.onClose();
+    });
+  }
 
-    onSidebarVisibility = () => {
-      console.log("click");
-      // EMIT A CHANGE IN THE SIDEBAR (IN OR OUT)
-      window.emitter.emit('setSidebarVisiblity', "CLOSE");  
+  onSidebarVisibility = () => {
+    console.log("click");
+    // EMIT A CHANGE IN THE SIDEBAR (IN OR OUT)
+    window.emitter.emit("setSidebarVisiblity", "CLOSE");
 
-      if (this.props.onSidebarVisibility !==  undefined)
-        this.props.onSidebarVisibility();
-    }
+    if (this.props.onSidebarVisibility !== undefined) this.props.onSidebarVisibility();
+  };
 
-    render() { 
-        return ( 
-            <React.Fragment>
-                <div id="sc-panel-component-header">
-                    <div id="sc-panel-component-title">
-                        <div id="sc-panel-component-title-container">
-                            <div id="sc-panel-component-tool-icon"> 
-                                <img src={images["tools-icon.png"]} alt="Theme"></img>
-                            </div>
-                            <div id="sc-panel-component-tool-text">{this.props.name}</div> 
-                            <div id="sc-panel-component-tool-controls"> 
-                                <img id="sc-panel-component-tool-img" src={images["tab-close-24x24.png"]} alt="Close Tab" onClick={this.onSidebarVisibility}></img> 
-                                <img id="sc-panel-component-tool-close" src={images["close-x-24x24.png"]} alt= "Close Tool" onClick={this.props.onClose}></img> 
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div id="sc-panel-component-content">{this.props.children}</div> 
-            </React.Fragment>
-        
-        );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <div id="sc-panel-component-header">
+          <div id="sc-panel-component-title">
+            <div id="sc-panel-component-title-container">
+              <div id="sc-panel-component-tool-icon">
+                <img src={images["tools-icon.png"]} alt="Theme"></img>
+              </div>
+              <div id="sc-panel-component-tool-text" className={this.props.name.length < 25 ? "sc-panel-component-tool-text" : "sc-panel-component-tool-text small"}>
+                {this.props.name}
+              </div>
+              <div id="sc-panel-component-tool-controls">
+                <img id="sc-panel-component-tool-img" src={images["tab-close-24x24.png"]} alt="Close Tab" onClick={this.onSidebarVisibility}></img>
+                <img id="sc-panel-component-tool-close" src={images["close-x-24x24.png"]} alt="Close Tool" onClick={this.props.onClose}></img>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div id="sc-panel-component-content">{this.props.children}</div>
+      </React.Fragment>
+    );
+  }
 }
- 
+
 export default PanelComponent;
 
 // IMPORT ALL IMAGES
-const images = importAllImages(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+const images = importAllImages(require.context("./images", false, /\.(png|jpe?g|svg)$/));
 function importAllImages(r) {
-    let images = {};
-    r.keys().map((item, index) => images[item.replace('./', '')] = r(item));
-    return images;
-  }
+  let images = {};
+  r.keys().map((item, index) => (images[item.replace("./", "")] = r(item)));
+  return images;
+}
--- src/sidebar/SidebarItemList.jsx
+++ src/sidebar/SidebarItemList.jsx
@@ -1,92 +1,85 @@
-import React, { Component } from 'react';
-import * as helpers from '../helpers/helpers';
+import React, { Component } from "react";
+import * as helpers from "../helpers/helpers";
 import "./SidebarItemList.css";
-import ComponentsConfig from '../config.json';
+import ComponentsConfig from "../config.json";
 
 class SidebarItemList extends Component {
-        
-  constructor(props){
+  constructor(props) {
     super(props);
 
     // BIND THIS TO THE CLICK FUNCTION
     this.buttonClick = this.buttonClick.bind(this);
 
     this.state = {
-        components: []
-    }
+      components: []
+    };
   }
 
-  buttonClick(name){
+  buttonClick(name) {
     //PARENT HANDLES THE REST
     this.props.onTabClick(name);
   }
 
-  render() { 
-
+  render() {
     // GET LIST OF COMPONENTS FROM CONFIG
     let listItems = null;
-    if (this.props.listtype === "tools"){
-        listItems = ComponentsConfig.sidebarToolComponents;
-    }
-    else{
-        listItems = ComponentsConfig.sidebarThemeComponents;
+    if (this.props.listtype === "tools") {
+      listItems = ComponentsConfig.sidebarToolComponents;
+    } else {
+      listItems = ComponentsConfig.sidebarThemeComponents;
     }
 
-    if (listItems === undefined)
-                return <div></div>
-                
-    return ( 
-      <React.Fragment>{
-          // CREATE ITEMS FROM CONFIG
-          listItems.map(listItem => 
+    if (listItems === undefined) return <div></div>;
 
-              (
-              <ToolItem componentname={listItem.componentName} 
-              onClick={()=>this.props.onClick(listItem.name, this.props.listtype)} 
-              key={helpers.getUID()} 
-              id={listItem.id} 
-              name={listItem.name} 
-              imageName={listItem.imageName} 
-              description={listItem.description} />
-              )
-          )}
-              
-      </React.Fragment>   
-    )
+    return (
+      <div className="simcoe-sidebarlist-container">
+        {// CREATE ITEMS FROM CONFIG
+        listItems.map(listItem => (
+          <ToolItem
+            componentname={listItem.componentName}
+            onClick={() => this.props.onClick(listItem.name, this.props.listtype)}
+            key={helpers.getUID()}
+            id={listItem.id}
+            name={listItem.name}
+            imageName={listItem.imageName}
+            description={listItem.description}
+          />
+        ))}
+      </div>
+    );
   }
 }
- 
+
 export default SidebarItemList;
 
 // ITEM
 class ToolItem extends Component {
-    state = {  }
-    
-    render() { 
-        return ( 
-            
-        <div className="simcoe-sidebarlist-item" onClick={this.props.onClick}>
-            <div className="simcoe-sidebarlist-item-iconbackground">
-                <div className="simcoe-sidebarlist-item-icon"></div>
-                <img src={images[this.props.imageName]} alt="Tool Item"/>
-                
-                {/* THIS WORKS IN DEV BUT PRODUCTION (NPM RUN BUILD) WEBPACK DOES NOT LIKE IT!!!!  
+  state = {};
+
+  render() {
+    return (
+      <div className="simcoe-sidebarlist-item" onClick={this.props.onClick}>
+        <div className="simcoe-sidebarlist-item-iconbackground">
+          <div className="simcoe-sidebarlist-item-icon"></div>
+          <img src={images[this.props.imageName]} alt="Tool Item" />
+
+          {/* THIS WORKS IN DEV BUT PRODUCTION (NPM RUN BUILD) WEBPACK DOES NOT LIKE IT!!!!  
                 //IMAGE ICONS FOR THE TOOLS LIST ARE NOW STORED IN SIDEBAR IMAGES FOLDER */}
-                    {/* <img src={require(`${this.props.imageURL}`)} ></img> */}
-                </div>
-            <div className="simcoe-sidebarlist-item-text-container">
-                <div className="simcoe-sidebarlist-item-text-title">{this.props.name}</div>
-                <div className="simcoe-sidebarlist-item-text-description">{this.props.description}</div>
-            </div>
-        </div> 
+          {/* <img src={require(`${this.props.imageURL}`)} ></img> */}
+        </div>
+        <div className="simcoe-sidebarlist-item-text-container">
+          <div className="simcoe-sidebarlist-item-text-title">{this.props.name}</div>
+          <div className="simcoe-sidebarlist-item-text-description">{this.props.description}</div>
+        </div>
+      </div>
     );
-    }
+  }
 }
 
 //IMPORT ALL IMAGES
-const images = importAllImages(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+const images = importAllImages(require.context("./images", false, /\.(png|jpe?g|svg)$/));
 function importAllImages(r) {
-    let images = {};
-    r.keys().map((item, index) => images[item.replace('./', '')] = r(item));
-    return images;
-  }
+  let images = {};
+  r.keys().map((item, index) => (images[item.replace("./", "")] = r(item)));
+  return images;
+}
