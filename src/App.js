783c1d866e0264d5547c051998c3f63917b758d3
Tom Reed <58078313+reed-tom@users.noreply.github.com>
2020-01-03 14:00:12 -0500

    Added config value for page title

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Header from "./header/Header";
 import Sidebar from "./sidebar/Sidebar";
 import SCMap from "./map/SCMap";
 import "./helpers/SC.css";
+import mainConfig from "./config.json";
 import ReactGA from "react-ga";
 ReactGA.initialize("UA-3104541-53");
 ReactGA.pageview(window.location.pathname + window.location.search);
 
 class App extends Component {
-
+  componentWillMount() {
+    document.title = mainConfig.title;
+  };
   render() {
     return (
       <div>
