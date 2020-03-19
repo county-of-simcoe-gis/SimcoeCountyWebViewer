6079c19d8f519db7c0c5d0c09dbcf9566cdab4b5
Tom Reed <58078313+reed-tom@users.noreply.github.com>
2020-03-19 13:11:19 -0400

    Added image url handling for document links

--- src/helpers/InfoRow.jsx
+++ src/helpers/InfoRow.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import mainConfig from "../config.json";
 export const InfoRow = props => {
   // CONVERT URL'S TO LINKS
   let value = props.value;
@@ -9,12 +9,28 @@ export const InfoRow = props => {
       .toString()
       .substring(0, 4)
       .toUpperCase() === "HTTP"
-  )
-    value = (
-      <a href={props.value} target="_blank" rel="noopener noreferrer">
-        Click To Open
-      </a>
-    );
+    )
+    {
+      value = (
+        <a href={props.value} target="_blank" rel="noopener noreferrer">
+          Click To Open
+        </a>
+      );
+    } else if (
+      props.value != null &&
+      props.value
+        .toString()
+        .substring(0, 2)
+        .toUpperCase() === "\\\\"
+      )
+      {
+        value = (
+          <a href={mainConfig.imageViewerUrl + props.value} target="_blank" rel="noopener noreferrer">
+            Click To Open
+          </a>
+        );
+      }
+    
 
   return (
     <div className="sc-info-window-row">
