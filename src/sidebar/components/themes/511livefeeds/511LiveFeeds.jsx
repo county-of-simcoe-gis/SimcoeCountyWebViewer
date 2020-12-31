9c3948d54d7d1813397f774a3ecdd6dd9683454f
Al Proulx <alain.proulx@hotmail.com>
2020-12-31 14:53:56 -0500

    Added Theme - 511 Live Feeds

---
+++ src/sidebar/components/themes/511livefeeds/511LiveFeeds.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./511LiveFeeds.css";
+import * as helpers from "../../../../helpers/helpers";
+import PanelComponent from "../../../PanelComponent";
+
+class ThemeComponent extends Component {
+  state = {};
+
+  onClose = () => {
+    // ADD CLEAN UP HERE (e.g. Map Layers, Popups, etc)
+
+    // CALL PARENT WITH CLOSE
+    this.props.onClose();
+  };
+
+  render() {
+    return (
+      <PanelComponent onClose={this.onClose} name={this.props.name} type="themes">
+        <div>Put your components in here.</div>
+      </PanelComponent>
+    );
+  }
+}
+
+export default ThemeComponent;
