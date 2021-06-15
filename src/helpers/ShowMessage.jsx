747239b28db4c81d7d39deb231aae26d1c0b18ed
Tom <58078313+reed-tom@users.noreply.github.com>
2021-06-15 13:09:30 -0400

    - switched search, mymaps, and local real estate to use localStorage helpers - added cleanup function to remove full geojson from saved search history - disabled saving of layers added from files - fixed error in show message event listener - extensive "prettier" formatting changes

--- src/helpers/ShowMessage.jsx
+++ src/helpers/ShowMessage.jsx
@@ -1,43 +1,62 @@
 import React, { Component } from "react";
 import "./ShowMessage.css";
 class ShowMessage extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.myRef = React.createRef();
+		this.myRef = React.createRef();
 
-    this.state = {
-      hide: false,
-      color: this.props.color ? this.props.color : "green",
-      sidebarOpen: window.sidebarOpen
-    };
-    window.emitter.addListener("sidebarChanged", (isSidebarOpen) => this.setState({sidebarOpen:isSidebarOpen}));
+		this.state = {
+			hide: false,
+			color: this.props.color ? this.props.color : "green",
+			sidebarOpen: window.sidebarOpen,
+		};
 
-  }
+		this._isMounted = false;
+	}
 
-  
-  onCloseClick = (value) => {
-    this.setState({ hide: true });
-  };
+	componentDidMount() {
+		this.sidebarEmitter = window.emitter.addListener(
+			"sidebarChanged",
+			(isSidebarOpen) => this.setState({ sidebarOpen: isSidebarOpen })
+		);
+		this._isMounted = true;
+	}
+	componentWillUnmount() {
+		this.sidebarEmitter.remove();
 
-  getClassName = () => {
-    if (this.state.hide) return "sc-hidden";
-    else if (this.state.sidebarOpen) return "sc-show-message-container " + this.state.color;
-    else return "sc-show-message-container closed " + this.state.color;
-  };
+		this._isMounted = false;
+	}
+	onCloseClick = (value) => {
+		if (this._isMounted) this.setState({ hide: true });
+	};
 
-  render() {
-    const style = this.getClassName();
-    return (
-      <div ref={this.myRef} id="sc-show-message-container" className={style}>
-        <div className="sc-show-message-title">{this.props.title}</div>
-        <div className="sc-show-message-content">{this.props.message}</div>
-        <button className={this.props.hideButton ? "sc-hidden" : "sc-button sc-show-message-close-button"} onClick={this.onCloseClick}>
-          Close Message
-        </button>
-      </div>
-    );
-  }
+	getClassName = () => {
+		if (this.state.hide) return "sc-hidden";
+		else if (this.state.sidebarOpen)
+			return "sc-show-message-container " + this.state.color;
+		else return "sc-show-message-container closed " + this.state.color;
+	};
+
+	render() {
+		const style = this.getClassName();
+		return (
+			<div ref={this.myRef} id="sc-show-message-container" className={style}>
+				<div className="sc-show-message-title">{this.props.title}</div>
+				<div className="sc-show-message-content">{this.props.message}</div>
+				<button
+					className={
+						this.props.hideButton
+							? "sc-hidden"
+							: "sc-button sc-show-message-close-button"
+					}
+					onClick={this.onCloseClick}
+				>
+					Close Message
+				</button>
+			</div>
+		);
+	}
 }
 
 export default ShowMessage;
