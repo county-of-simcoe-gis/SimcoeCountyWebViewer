69e056fcb22a7a71e6f459e51268e6843c558e68
Tom <58078313+reed-tom@users.noreply.github.com>
2021-10-15 14:56:24 -0400

    fixed layer toggle on commercial real estate theme (#157)

--- src/sidebar/components/themes/commercialrealestate/CommercialRealEstateLayers.jsx
+++ src/sidebar/components/themes/commercialrealestate/CommercialRealEstateLayers.jsx
@@ -3,16 +3,16 @@ import CommercialRealEstateLayerToggler from "./CommercialRealEstateLayerToggler
 import * as helpers from "../../../../helpers/helpers";
 import "./CommercialRealEstateLayers.css";
 const CommercialRealEstateLayers = React.memo((props) => {
-	return (
-		<div className="sc-theme-commercial-real-estate-layers-container">
-			<div className="sc-title">Layers</div>
-			<div className="sc-border-top" style={{ paddingBottom: "5px" }}>
-				{props.layers.map((layer) => {
-					return <CommercialRealEstateLayerToggler key={helpers.getUID()} layer={layer} />;
-				})}
-			</div>
-		</div>
-	);
+  return (
+    <div className="sc-theme-commercial-real-estate-layers-container">
+      <div className="sc-title">Layers</div>
+      <div className="sc-border-top" style={{ paddingBottom: "5px" }}>
+        {props.layers.map((layer) => {
+          return <CommercialRealEstateLayerToggler key={`sc-theme-commercial-real-estate-layers-${layer.layerName}`} layer={layer} />;
+        })}
+      </div>
+    </div>
+  );
 });
 
 export default CommercialRealEstateLayers;
