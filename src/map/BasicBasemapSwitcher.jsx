3121c5e39b6160f7ada005a930a354b5f817fbd3
Nedim Oren <nedim.oren@gmail.com>
2021-11-09 22:56:30 -0500

    VectorTile support for ol-mapbox-sytle styleFunction is added

--- src/map/BasicBasemapSwitcher.jsx
+++ src/map/BasicBasemapSwitcher.jsx
@@ -69,6 +69,8 @@ class BasicBasemapSwitcher extends Component {
           layer = helpers.getESRITileXYZLayer(service.url);
         }else if (service.type === "XYZ") {
           layer = helpers.getXYZLayer(service.url);
+        }else if (service.type === "ESRI_VECTOR_TILED") {
+          layer = helpers.getVectorTileLayer(service.url);
         }else{
           layer = new VectorLayer({source: new VectorSource()});
         }
