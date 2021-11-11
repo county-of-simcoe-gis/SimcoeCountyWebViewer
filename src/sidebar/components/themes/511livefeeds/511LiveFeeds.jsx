e36d6dc5ce43b5dad72e10ccaa30da091815032b
Tom <58078313+reed-tom@users.noreply.github.com>
2021-11-11 16:00:04 -0500

    added ability to override the sidebar tabs for themes or tools if only one component is available

--- src/sidebar/components/themes/511livefeeds/511LiveFeeds.jsx
+++ src/sidebar/components/themes/511livefeeds/511LiveFeeds.jsx
@@ -4,22 +4,22 @@ import * as helpers from "../../../../helpers/helpers";
 import PanelComponent from "../../../PanelComponent";
 
 class ThemeComponent extends Component {
-	state = {};
+  state = {};
 
-	onClose = () => {
-		// ADD CLEAN UP HERE (e.g. Map Layers, Popups, etc)
+  onClose = () => {
+    // ADD CLEAN UP HERE (e.g. Map Layers, Popups, etc)
 
-		// CALL PARENT WITH CLOSE
-		this.props.onClose();
-	};
+    // CALL PARENT WITH CLOSE
+    this.props.onClose();
+  };
 
-	render() {
-		return (
-			<PanelComponent onClose={this.onClose} name={this.props.name} helpLink={this.props.helpLink} type="themes">
-				<div>Put your components in here.</div>
-			</PanelComponent>
-		);
-	}
+  render() {
+    return (
+      <PanelComponent onClose={this.onClose} name={this.props.name} helpLink={this.props.helpLink} hideHeader={this.props.hideHeader} type="themes">
+        <div>Put your components in here.</div>
+      </PanelComponent>
+    );
+  }
 }
 
 export default ThemeComponent;
--- src/sidebar/components/tools/searchhistory/SearchHistory.jsx
+++ src/sidebar/components/tools/searchhistory/SearchHistory.jsx
@@ -4,134 +4,134 @@ import * as helpers from "../../../../helpers/helpers";
 import PanelComponent from "../../../PanelComponent";
 const storageKey = "searchHistory";
 class ToolComponent extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = { items: [] };
-	}
+    this.state = { items: [] };
+  }
 
-	onClose = () => {
-		// ADD CLEAN UP HERE (e.g. Map Layers, Popups, etc)
+  onClose = () => {
+    // ADD CLEAN UP HERE (e.g. Map Layers, Popups, etc)
 
-		// CALL PARENT WITH CLOSE
-		this.props.onClose();
-	};
+    // CALL PARENT WITH CLOSE
+    this.props.onClose();
+  };
 
-	componentDidMount() {
-		this.getStorage();
+  componentDidMount() {
+    this.getStorage();
 
-		// LISTEN FOR SEARCHS
-		window.emitter.addListener("searchComplete", () => this.getStorage());
-	}
+    // LISTEN FOR SEARCHS
+    window.emitter.addListener("searchComplete", () => this.getStorage());
+  }
 
-	// GET STORAGE
-	getStorage() {
-		const data = helpers.getItemsFromStorage(storageKey);
-		if (data !== undefined) this.setState({ items: data });
-	}
+  // GET STORAGE
+  getStorage() {
+    const data = helpers.getItemsFromStorage(storageKey);
+    if (data !== undefined) this.setState({ items: data });
+  }
 
-	saveStateToStorage = () => {
-		helpers.saveToStorage(storageKey, this.state.items);
-	};
+  saveStateToStorage = () => {
+    helpers.saveToStorage(storageKey, this.state.items);
+  };
 
-	onMoreInfoClick = (item) => {
-		window.open(item.ReportURL, "_blank");
-	};
+  onMoreInfoClick = (item) => {
+    window.open(item.ReportURL, "_blank");
+  };
 
-	onReplayClick = (item) => {
-		window.emitter.emit("searchHistorySelect", item);
-	};
-	onZoomClick = (item) => {
-		helpers.zoomToFeature(helpers.getFeatureFromGeoJSON(item.geojson_extent));
-	};
+  onReplayClick = (item) => {
+    window.emitter.emit("searchHistorySelect", item);
+  };
+  onZoomClick = (item) => {
+    helpers.zoomToFeature(helpers.getFeatureFromGeoJSON(item.geojson_extent));
+  };
 
-	onRemoveClick = (item) => {
-		this.setState(
-			{
-				items: this.state.items.filter(function (itemInfo) {
-					return itemInfo.id !== item.id;
-				}),
-			},
-			() => {
-				// UPDATE STORAGE
-				this.saveStateToStorage();
-			}
-		);
-	};
+  onRemoveClick = (item) => {
+    this.setState(
+      {
+        items: this.state.items.filter(function (itemInfo) {
+          return itemInfo.id !== item.id;
+        }),
+      },
+      () => {
+        // UPDATE STORAGE
+        this.saveStateToStorage();
+      }
+    );
+  };
 
-	onDeleteAll = () => {
-		this.setState({ items: [] }, () => {
-			this.saveStateToStorage();
-		});
-	};
+  onDeleteAll = () => {
+    this.setState({ items: [] }, () => {
+      this.saveStateToStorage();
+    });
+  };
 
-	render() {
-		return (
-			<PanelComponent onClose={this.onClose} name={this.props.name} helpLink={this.props.helpLink} type="tools">
-				<div className="sc-tool-search-history-main-container">
-					<div style={{ fontSize: "9pt" }}>Below is a list of your most recent searched items. These items will be added automatically after each search you do for future reference.</div>
-					<div className={this.state.items.length === 0 ? "sc-tool-search-history-no-results" : "sc-hidden"}>Your search history is currently empty. Once you search an item, it will appear here.</div>
-					<div className={this.state.items.length > 0 ? "sc-tool-search-history-results" : "sc-hidden"}>
-						{this.state.items.map((item) => {
-							return (
-								<SearchItem
-									key={helpers.getUID()}
-									item={item}
-									onMoreInfoClick={this.onMoreInfoClick}
-									onReplayClick={this.onReplayClick}
-									onRemoveClick={this.onRemoveClick}
-									onZoomClick={this.onZoomClick}
-								></SearchItem>
-							);
-						})}
-					</div>
-					<div className={this.state.items.length === 0 ? "sc-hidden" : "sc-tool-search-history-footer"}>
-						<button className="sc-button" style={{ width: "200px", marginTop: "4px" }} onClick={this.onDeleteAll}>
-							{"Clear History - (" + this.state.items.length + " of 25)"}
-						</button>
-					</div>
-				</div>
-			</PanelComponent>
-		);
-	}
+  render() {
+    return (
+      <PanelComponent onClose={this.onClose} name={this.props.name} helpLink={this.props.helpLink} hideHeader={this.props.hideHeader} type="tools">
+        <div className="sc-tool-search-history-main-container">
+          <div style={{ fontSize: "9pt" }}>Below is a list of your most recent searched items. These items will be added automatically after each search you do for future reference.</div>
+          <div className={this.state.items.length === 0 ? "sc-tool-search-history-no-results" : "sc-hidden"}>Your search history is currently empty. Once you search an item, it will appear here.</div>
+          <div className={this.state.items.length > 0 ? "sc-tool-search-history-results" : "sc-hidden"}>
+            {this.state.items.map((item) => {
+              return (
+                <SearchItem
+                  key={helpers.getUID()}
+                  item={item}
+                  onMoreInfoClick={this.onMoreInfoClick}
+                  onReplayClick={this.onReplayClick}
+                  onRemoveClick={this.onRemoveClick}
+                  onZoomClick={this.onZoomClick}
+                ></SearchItem>
+              );
+            })}
+          </div>
+          <div className={this.state.items.length === 0 ? "sc-hidden" : "sc-tool-search-history-footer"}>
+            <button className="sc-button" style={{ width: "200px", marginTop: "4px" }} onClick={this.onDeleteAll}>
+              {"Clear History - (" + this.state.items.length + " of 25)"}
+            </button>
+          </div>
+        </div>
+      </PanelComponent>
+    );
+  }
 }
 
 export default ToolComponent;
 
 const SearchItem = (props) => {
-	return (
-		<div className="sc-container sc-tool-search-history-item-container">
-			<div className="sc-tool-search-history-image">
-				<img src={images["map-marker-light-blue.png"]} alt="Map Marker" />
-			</div>
-			<div className="sc-tool-search-history-details-name">{props.item.name}</div>
-			<div className="sc-tool-search-history-details-muni">{"- (" + props.item.type + ")"}</div>
-			<div className="sc-tool-search-history-details-date">{"Added: " + props.item.dateAdded}</div>
-			{/* <div className="sc-tool-search-history-divider"></div> */}
-			<div className="sc-tool-search-history-background" />
-			<div
-				className={props.item.Type === "Address" || props.item.Type === "Assessment Parcel" ? "sc-fakeLink sc-tool-search-history-button-info" : "sc-hidden"}
-				onClick={() => props.onMoreInfoClick(props.item)}
-			>
-				More Information
-			</div>
-			<div className="sc-fakeLink sc-tool-search-history-button-replay" onClick={() => props.onReplayClick(props.item)}>
-				Replay Search
-			</div>
-			<div className="sc-fakeLink sc-tool-search-history-button-remove" onClick={() => props.onRemoveClick(props.item)}>
-				Remove
-			</div>
-			<div className="sc-fakeLink sc-tool-search-history-button-zoom" onClick={() => props.onZoomClick(props.item)}>
-				Zoom
-			</div>
-		</div>
-	);
+  return (
+    <div className="sc-container sc-tool-search-history-item-container">
+      <div className="sc-tool-search-history-image">
+        <img src={images["map-marker-light-blue.png"]} alt="Map Marker" />
+      </div>
+      <div className="sc-tool-search-history-details-name">{props.item.name}</div>
+      <div className="sc-tool-search-history-details-muni">{"- (" + props.item.type + ")"}</div>
+      <div className="sc-tool-search-history-details-date">{"Added: " + props.item.dateAdded}</div>
+      {/* <div className="sc-tool-search-history-divider"></div> */}
+      <div className="sc-tool-search-history-background" />
+      <div
+        className={props.item.Type === "Address" || props.item.Type === "Assessment Parcel" ? "sc-fakeLink sc-tool-search-history-button-info" : "sc-hidden"}
+        onClick={() => props.onMoreInfoClick(props.item)}
+      >
+        More Information
+      </div>
+      <div className="sc-fakeLink sc-tool-search-history-button-replay" onClick={() => props.onReplayClick(props.item)}>
+        Replay Search
+      </div>
+      <div className="sc-fakeLink sc-tool-search-history-button-remove" onClick={() => props.onRemoveClick(props.item)}>
+        Remove
+      </div>
+      <div className="sc-fakeLink sc-tool-search-history-button-zoom" onClick={() => props.onZoomClick(props.item)}>
+        Zoom
+      </div>
+    </div>
+  );
 };
 
 // IMPORT ALL IMAGES
 const images = importAllImages(require.context("./images", false, /\.(png|jpe?g|svg|gif)$/));
 function importAllImages(r) {
-	let images = {};
-	r.keys().map((item, index) => (images[item.replace("./", "")] = r(item)));
-	return images;
+  let images = {};
+  r.keys().map((item, index) => (images[item.replace("./", "")] = r(item)));
+  return images;
 }
