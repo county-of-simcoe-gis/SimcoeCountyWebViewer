105a51ce9ed2b029db595f9d4601910094b89c36
Tom <58078313+reed-tom@users.noreply.github.com>
2022-03-16 14:45:31 -0400

    added check for featureHighlitStyles existance in search before referencing strokeWidth

--- src/header/Search.jsx
+++ src/header/Search.jsx
@@ -394,7 +394,9 @@ class Search extends Component {
           ? window.config.featureHighlitStyles["stroke"]
           : [255, 0, 0, 0.8],
         false,
-        window.config.featureHighlitStyles["strokeWidth"] !== null && window.config.featureHighlitStyles["strokeWidth"] !== undefined ? window.config.featureHighlitStyles["strokeWidth"] : 2,
+        window.config.featureHighlitStyles && window.config.featureHighlitStyles["strokeWidth"] !== null && window.config.featureHighlitStyles["strokeWidth"] !== undefined
+          ? window.config.featureHighlitStyles["strokeWidth"]
+          : 2,
         fullFeature.getGeometry().getType()
       );
       defaultStyle.setFill(
