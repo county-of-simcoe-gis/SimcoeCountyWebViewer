4f87f227cf10d9fe00bd5b0bcddad5cab75a18fb
Tom <58078313+reed-tom@users.noreply.github.com>
2023-06-30 14:41:45 -0400

    converted MyMapsPopup to functional component

--- src/sidebar/components/mymaps/MyMapsPopup.jsx
+++ src/sidebar/components/mymaps/MyMapsPopup.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useRef, useEffect } from "react";
 import MyMapsSymbolizer from "./MyMapsSymbolizer.jsx";
 import MyMapsBuffer from "./MyMapsBuffer";
 import MyMapsMeasure from "./MyMapsMeasure";
@@ -6,65 +6,50 @@ import MyMapsPopupLabel from "./MyMapsPopupLabel";
 import "./MyMapsPopup.css";
 import * as helpers from "../../../helpers/helpers";
 
-class MyMapsPopup extends Component {
-  state = {};
+const MyMapsPopup = (props) => {
+  const popupLabelRef = useRef(null);
+  useEffect(() => {
+    props.onRef(this);
+    return () => {
+      props.onRef(undefined);
+    };
+  }, []);
 
-  componentDidMount() {
-    this.props.onRef(this);
-  }
-  componentWillUnmount() {
-    this.props.onRef(undefined);
-  }
-
-  componentWillReceiveProps(nextProps) {
-    // this.popupLabelRef.forceUpdate();
-  }
-
-  parentLabelChanged = (itemInfo, newLabel) => {
-    this.popupLabelRef.parentLabelChange(itemInfo, newLabel);
-  };
-
-  parentLabelVisibleChanged = (itemInfo, visible) => {
-    this.popupLabelRef.parentLabelVisibilityChange(itemInfo, visible);
-  };
-
-  render() {
-    return (
-      <div className="sc-mymaps-popup-container">
-        <MyMapsPopupLabel
-          onRef={(ref) => (this.popupLabelRef = ref)}
-          item={this.props.item}
-          onLabelChange={this.props.onLabelChange}
-          onLabelVisibilityChange={this.props.onLabelVisibilityChange}
-          onLabelRotationChange={this.props.onLabelRotationChange}
-        />
-        <MyMapsSymbolizer
-          key={helpers.getUID()}
-          visible={this.props.activeTool === "symbolizer"}
-          item={this.props.item}
-          onPointStyleDropDown={this.props.onPointStyleDropDown}
-          onRadiusSliderChange={this.props.onRadiusSliderChange}
-          onFillColorPickerChange={this.props.onFillColorPickerChange}
-          onFillOpacitySliderChange={this.props.onFillOpacitySliderChange}
-          onRotationSliderChange={this.props.onRotationSliderChange}
-          onStrokeOpacitySliderChange={this.props.onStrokeOpacitySliderChange}
-          onStrokeColorPickerChange={this.props.onStrokeColorPickerChange}
-          onStrokeWidthSliderChange={this.props.onStrokeWidthSliderChange}
-          onStrokeTypeDropDown={this.props.onStrokeTypeDropDown}
-        />
-        <MyMapsBuffer visible={this.props.activeTool === "buffer"} item={this.props.item} />
-        <MyMapsMeasure visible={this.props.activeTool === "measure"} item={this.props.item} />
-        <FooterButtons
-          onMyMapItemToolsButtonClick={(evt) => this.props.onMyMapItemToolsButtonClick(evt, this.props.item)}
-          onDeleteButtonClick={() => {
-            this.props.onDeleteButtonClick(this.props.item.id);
-            window.popup.hide();
-          }}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="sc-mymaps-popup-container">
+      <MyMapsPopupLabel
+        onRef={(ref) => (popupLabelRef.current = ref)}
+        item={props.item}
+        onLabelChange={props.onLabelChange}
+        onLabelVisibilityChange={props.onLabelVisibilityChange}
+        onLabelRotationChange={props.onLabelRotationChange}
+      />
+      <MyMapsSymbolizer
+        key={helpers.getUID()}
+        visible={props.activeTool === "symbolizer"}
+        item={props.item}
+        onPointStyleDropDown={props.onPointStyleDropDown}
+        onRadiusSliderChange={props.onRadiusSliderChange}
+        onFillColorPickerChange={props.onFillColorPickerChange}
+        onFillOpacitySliderChange={props.onFillOpacitySliderChange}
+        onRotationSliderChange={props.onRotationSliderChange}
+        onStrokeOpacitySliderChange={props.onStrokeOpacitySliderChange}
+        onStrokeColorPickerChange={props.onStrokeColorPickerChange}
+        onStrokeWidthSliderChange={props.onStrokeWidthSliderChange}
+        onStrokeTypeDropDown={props.onStrokeTypeDropDown}
+      />
+      <MyMapsBuffer visible={props.activeTool === "buffer"} item={props.item} />
+      <MyMapsMeasure visible={props.activeTool === "measure"} item={props.item} />
+      <FooterButtons
+        onMyMapItemToolsButtonClick={(evt) => props.onMyMapItemToolsButtonClick(evt, props.item)}
+        onDeleteButtonClick={() => {
+          this.props.onDeleteButtonClick(props.item.id);
+          window.popup.hide();
+        }}
+      />
+    </div>
+  );
+};
 
 export default MyMapsPopup;
 
