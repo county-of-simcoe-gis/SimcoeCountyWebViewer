b833873a7e3dcd55cee7141cde46061f6fdee5e0
Tom <58078313+reed-tom@users.noreply.github.com>
2023-10-04 11:10:48 -0400

    removed unused helper

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -1134,22 +1134,6 @@ export function getWKTStringFromFeature(feature) {
   // return feature;
 }
 
-export function formatReplace(fmt, ...args) {
-  if (!fmt.match(/^(?:(?:(?:[^{}]|(?:\{\{)|(?:\}\}))+)|(?:\{[0-9]+\}))+$/)) {
-    throw new Error("invalid format string.");
-  }
-  return fmt.replace(/((?:[^{}]|(?:\{\{)|(?:\}\}))+)|(?:\{([0-9]+)\})/g, (m, str, index) => {
-    if (str) {
-      return str.replace(/(?:{{)|(?:}})/g, (m) => m[0]);
-    } else {
-      if (index >= args.length) {
-        throw new Error("argument index is out of range in format");
-      }
-      return args[index];
-    }
-  });
-}
-
 export function toLatLongFromWebMercator(coords) {
   return transform(coords, "EPSG:3857", "EPSG:4326");
 }
