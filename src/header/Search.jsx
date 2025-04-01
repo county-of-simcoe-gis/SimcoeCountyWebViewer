a597feb1b3f24efaba55ddfaae6c50ddaeca1fa2
Tom Reed <Tom.Reed@simcoe.ca>
2025-04-01 14:05:53 -0400

    added option to hide search

--- src/header/Search.jsx
+++ src/header/Search.jsx
@@ -47,6 +47,7 @@ const Search = (props) => {
   const [municipality, setMunicipality] = useState(undefined);
   const [placeHolderText, setPlaceHolderText] = useState("Search...");
   const [hideTypeDropDown, setHideTypeDropDown] = useState(false);
+  const [hideSearch, setHideSearch] = useState(false);
   const searchResultsRef = useRef([]);
   // VECTOR LAYERS
   const searchGeoLayerRef = useRef(null);
@@ -153,6 +154,7 @@ const Search = (props) => {
       if (window.config.search) {
         if (window.config.search.placeHolder !== undefined) setPlaceHolderText(window.config.search.placeHolder);
         if (window.config.search.hideTypes !== undefined) setHideTypeDropDown(window.config.search.hideTypes);
+        if (window.config.hideSearch !== undefined) setHideSearch(window.config.hideSearch);
       }
       helpers.getJSON(searchTypesURL(apiUrlRef.current), (result) => {
         let items = [];
@@ -760,7 +762,7 @@ const Search = (props) => {
   const [loading, setLoading] = useState(false);
 
   return (
-    <div>
+    <div className={hideSearch ? "sc-hidden" : ""}>
       <div className={hideTypeDropDown ? "sc-hidden" : "sc-search-types-container"} tabIndex="-1">
         <Select tabIndex="-1" styles={groupsDropDownStyles.current} isSearchable={false} onChange={onTypeDropDownChange} options={searchTypes} value={selectedType} />
       </div>
